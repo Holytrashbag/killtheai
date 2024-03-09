1fe1304b2ed53525a723a26c45049f5a9196d975
Moritz Wirth <theholytrashbag@gmail.com>
2024-03-09 21:32:24 +0100

    add font and create landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let gameId = '';
+</script>
+
+<main class="w-screen h-screen flex flex-col justify-center items-center gap-16 bg-gradient-to-br from-sky-400 to-purple-500 text-pink-50">
+    <div class="flex flex-col items-center">
+        <h1 class="text-9xl font-noto italic pb-8">Masterprompter</h1>
+        <p class="text-3xl font-noto">Prompt like a legend</p>
+    </div>
+    <div class="flex flex-col items-center gap-4">
+        <input bind:value={gameId} class="w-64 h-12 rounded-xl text-zinc-950 px-2 focus:outline-orange-400" placeholder="Join a game"/>
+        <button class="w-72 h-16 bg-gradient-to-tr rounded-xl from-orange-400 to-pink-400 disabled:saturate-[20%] text-3xl font-noto font-bold" disabled={gameId == ''}>Join Game</button>
+        <p>or</p>
+        <button class="w-72 h-16 bg-gradient-to-tr rounded-xl from-emerald-400 to-sky-400 text-3xl font-noto font-bold">Create Game</button>
+    </div>
+</main>
